639e5706ebac5e7a3918514f95a617761e307641
compunet <compunet@compunets-MacBook-Pro.local>
2024-07-29 13:55:42 +0100

    destined

--- js.js
+++ js.js
@@ -19,17 +19,55 @@ setInterval(function(){
 },10000)
 
 
-function workabeg(){
-    document.querySelector(".imgs").classList.add("active")
-    rrrr++
-}
+// function workabeg(){
+//     document.querySelector(".imgs").classList.add("active")
+//     rrrr++
+// }
 
-document.getElementById("nn1").addEventListener("click", function(){
+// document.getElementById("nn1").addEventListener("click", function(){
     
-    workabeg() = rrrr;
+//     workabeg() = rrrr;
+// })
+
+// document.getElementById("nn2").addEventListener("click", function(){
+
+//     document.querySelector("#radio1").classList.add("checked")
+// })
+
+document.querySelector(".play").addEventListener("click", function(){
+    // window.alert("kkkk")
+    document.querySelector(".over").classList.add("active")
+    document.querySelector(".popup").classList.add("active")
+    document.querySelector("body").classList.add("active")
+    document.getElementById("ul").classList.add("active")
+    document.querySelector(".top").classList.add("active")
 })
 
-document.getElementById("nn2").addEventListener("click", function(){
 
-    document.querySelector("#radio1").classList.add("checked")
+document.getElementById("overp").addEventListener("click", function(){
+    // window.alert("kkkk")
+    document.querySelector(".over").classList.remove("active")
+    document.querySelector(".popup").classList.remove("active")
+    document.querySelector("body").classList.remove("active")
+    document.getElementById("ul").classList.remove("active")
+    document.querySelector(".top").classList.remove("active")
+})
+
+
+document.getElementById("faqham").addEventListener("click", function(){
+    // window.alert("kdkdkdk")
+    document.getElementById("faqham").classList.add("active")
+    document.getElementById("faqp").classList.add("active")
+    document.getElementById("faqimg").classList.add("active")
+    document.querySelector(".faqs").classList.add("active")
+})
+
+
+document.getElementById("faqp").addEventListener("click", function(){
+    // window.alert("kdkdkdk")
+    document.getElementById("faqham").classList.remove
+    ("active")
+    document.getElementById("faqp").classList.remove("active")
+    document.getElementById("faqimg").classList.remove("active")
+    document.querySelector(".faqs").classList.remove("active")
 })
